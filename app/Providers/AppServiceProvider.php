9176a16eb69c55e447d184d57ffa90c2c4158ef3
rcpassos <r.passos@outlook.pt>
2025-03-07 23:31:24 +0000

    feat: add rules to configure project

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +25,35 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureDates();
+        $this->configureModels();
+        $this->configureUrl();
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            App::isProduction()
+        );
+    }
+
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    private function configureModels()
+    {
+        // throw an exception if access to a property that does not exist
+        Model::shouldBeStrict();
+
+        // disable mass assignment protection
+        // Model::unguard();
+    }
+
+    private function configureUrl(): void
+    {
+        URL::forceScheme('https');
     }
 }
