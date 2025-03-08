22a83edb3cc42b5d18a0960e8fdc4f10402f7516
rcpassos <r.passos@outlook.pt>
2025-03-08 00:06:17 +0000

    feat: add response model

---
+++ app/Models/Response.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Response extends Model
+{
+    /** @use HasFactory<\Database\Factories\ResponseFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'issue_id',
+        'user_id',
+        'content',
+    ];
+
+    /** @return BelongsTo<Issue, $this> */
+    public function issue()
+    {
+        return $this->belongsTo(Issue::class);
+    }
+
+    /** @return BelongsTo<User, $this> */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
